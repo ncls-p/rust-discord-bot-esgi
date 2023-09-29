404e2420636ce70aaa0ba9dcbabf83025bbc1bc3
Nicolas Pierrot <nicolas@archipels.io>
2023-09-29 14:29:28 +0200

    feat: infos command

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use anyhow::anyhow;
-use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
+use serenity::utils::Colour;
+use serenity::{async_trait, builder::CreateEmbed};
 use shuttle_secrets::SecretStore;
 use tracing::{error, info};
 
@@ -14,6 +15,26 @@ impl EventHandler for Bot {
         if msg.content == "!hello" {
             if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
                 error!("Error sending message: {:?}", e);
+            }
+        } else if msg.content == "!infos" {
+            let mut embed = CreateEmbed::default();
+            embed.title("Infos")
+                    .description("Infos sur le bot")
+                    .field("Créateur", "ncls-p", false)
+                    .field("Langage", "Rust", false)
+                    .field("Github", "https://github.com/ncls-p/IABD2.RS", false)
+                    .field("Version", "0.1.0", false)
+                    .field("Librairie", "Serenity", false)
+                    .color(Colour::PURPLE)
+                    .url("https://github.com/ncls-p/IABD2.RS")
+                    .image("https://cdn.discordapp.com/attachments/1028352036049277060/1156246329736044676/download.png?ex=651445cf&is=6512f44f&hm=19e23e3d7ec03497772c97daccaa35cbb978df8e4c104414c0c2e63c547091f0&");
+
+            if let Err(err) = msg
+                .channel_id
+                .send_message(&ctx.http, |m| m.set_embed(embed.clone()))
+                .await
+            {
+                eprintln!("Error sending info message: {:?}", err);
             }
         }
     }
